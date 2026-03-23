cec70a9868dccc8732a20f3fe8d2066c9db63567
denysolshanskiy-hue <denysolshanskiy@gmail.com>
2026-03-23 17:53:57 +0200

    Update keyboards.py

--- mafia_bot/modules/keyboards.py
+++ mafia_bot/modules/keyboards.py
@@ -1,1 +1,14 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
+def admin_menu_keyboard():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="➕ Створити івент"), KeyboardButton(text="📅 Активні події")],
+            [KeyboardButton(text="💳 Оплатити ігри"), KeyboardButton(text="🛠 Адмін: список + скасовані")],
+            [KeyboardButton(text="✅ Підтвердити вечір"), KeyboardButton(text="🏁 Завершити вечір")],
+            [KeyboardButton(text="❌ Скасувати івент")],
+            [KeyboardButton(text="☣️ UNDERGROUND")]
+        ],
+        resize_keyboard=True
+    )
