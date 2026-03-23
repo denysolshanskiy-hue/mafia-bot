78640be49ab1d7d94a35e8498028be31383b7ef6
denysolshanskiy-hue <denysolshanskiy@gmail.com>
2026-03-23 12:46:47 +0200

    Add Google Sheets integration for underground module
    Initialize Google Sheets integration with gspread and oauth2client.

--- mafia_bot/modules/underground/sheets.py
+++ mafia_bot/modules/underground/sheets.py
@@ -1,1 +1,21 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+SHEET_NAME = "UNDERGROUND"
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "credentials.json", scope
+)
+
+client = gspread.authorize(creds)
+
+spreadsheet = client.open(SHEET_NAME)
+
+players_sheet = spreadsheet.worksheet("Players")
+results_sheet = spreadsheet.worksheet("Results")
+events_sheet = spreadsheet.worksheet("Events")
